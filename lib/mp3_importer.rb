9d66f033d4732da000a61d958c3cbb08dac1ec49
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-07 02:31:35 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,25 @@
+class MP3Importer
+  
+  attr_accessor :path
+ 
+  def initialize(path)
+    @path = path
+  end
+  
+ def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+
+  
+  def import
+    files.each { |some_filename| Song.new_by_filename(some_filename) }
+  end
+
+
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  
+    @@all = []
+    
+  
+    attr_accessor :artist, :name
+
+    def initialize(name)
+       @name = name
+        @@all << self
+    end
+
+    def self.all
+     @@all
+    end
+
+    def self.new_by_filename(filename)
+        song = self.new(filename.split(" - ")[1])
+        song.artist = Artist.new(filename.split(" - ")[0])
+        song
+    end
+
+    def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+      
+    end
+  
+  
+end
+
+
